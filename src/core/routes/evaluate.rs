e46d5b0b6a8f3623ab04d57bc53d4689df4a73b8
Devon Stewart <devon@appreciate.it>
2022-11-17 13:25:09 -0800

    Static routing requests through the delegator

--- src/core/routes/evaluate.rs
+++ src/core/routes/evaluate.rs
@@ -18,7 +18,26 @@ pub struct JsonCryptogram {
 }
 
 pub async fn evaluate(cryptogram: Json<JsonCryptogram>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+    println!("Cryptogram: {:?}", cryptogram);
+
+    let client = awc::Client::default();
+
+    let mut final_result: Option<Value> = None;
+
+    for step in &cryptogram.steps {
+        let req = client
+            .post("http://localhost:4000/request-signup-token")
+            .insert_header(("User-Agent", "awc/3.0"))
+            .insert_header(("Content-Type", "application/json"))
+            .send_json(&step.payload.clone());
+        let mut res = req.await.unwrap();
+        let x = res.json::<Value>().await.unwrap();
+        final_result = Some(x);
+    }
+
+    final_result
+        .map(|res| HttpResponse::Ok().json(&res))
+        .unwrap_or(HttpResponse::BadRequest().finish())
 }
 
 pub fn configure(server: &mut web::ServiceConfig) {
