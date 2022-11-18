4e57747d3bf434f6148a155f1fba9a01303fac99
Devon Stewart <devon@appreciate.it>
2022-11-17 17:26:39 -0800

    Incorporating service definitions from config

--- src/core/routes/evaluate.rs
+++ src/core/routes/evaluate.rs
@@ -1,9 +1,12 @@
 use actix_web::{
-    web::{self, Json},
+    http::Uri,
+    web::{self, Data, Json},
     HttpResponse,
 };
 use serde::Deserialize;
 use serde_json::Value;
+
+use crate::config::{ServiceDefinition, Services};
 
 #[derive(Debug, Deserialize)]
 pub struct JsonCryptogramStep {
@@ -17,22 +20,37 @@ pub struct JsonCryptogram {
     steps: Vec<JsonCryptogramStep>,
 }
 
-pub async fn evaluate(cryptogram: Json<JsonCryptogram>) -> HttpResponse {
-    println!("Cryptogram: {:?}", cryptogram);
-
+pub async fn evaluate(cryptogram: Json<JsonCryptogram>, services: Data<Services>) -> HttpResponse {
     let client = awc::Client::default();
 
     let mut final_result: Option<Value> = None;
 
     for step in &cryptogram.steps {
-        let req = client
-            .post("http://localhost:4000/request-signup-token")
-            .insert_header(("User-Agent", "awc/3.0"))
-            .insert_header(("Content-Type", "application/json"))
-            .send_json(&step.payload.clone());
-        let mut res = req.await.unwrap();
-        let x = res.json::<Value>().await.unwrap();
-        final_result = Some(x);
+        let service = services.get(&step.service).unwrap().to_owned();
+        final_result = match service {
+            ServiceDefinition::Rest {
+                scheme,
+                endpoint,
+                methods,
+            } => {
+                let method = methods.get(&step.method).unwrap();
+
+                let uri = Uri::builder()
+                    .scheme(scheme)
+                    .authority(endpoint)
+                    .path_and_query(method.path.to_owned())
+                    .build()
+                    .unwrap();
+                let req = client
+                    .post(uri)
+                    .insert_header(("User-Agent", "awc/3.0"))
+                    .insert_header(("Content-Type", "application/json"))
+                    .send_json(&step.payload.clone());
+                let mut res = req.await.unwrap();
+                let x = res.json::<Value>().await.unwrap();
+                Some(x)
+            }
+        };
     }
 
     final_result
