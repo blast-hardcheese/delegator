06dd66617100c16fc1a74f78a1d543098adde9df
Devon Stewart <devon@appreciate.it>
2023-02-12 17:25:38 -0500

    Adding error handling

--- src/core/translate/mod.rs
+++ src/core/translate/mod.rs
@@ -1,4 +1,4 @@
-use serde_json::Value;
+use serde_json::{Map, Value};
 
 // translate
 //
@@ -12,24 +12,103 @@ pub enum Language {
     Object(Vec<(String, Language)>), // { foo: .foo, bar: .bar  }
 }
 
-pub fn step(prog: Language, current: &Value) -> Value {
+#[derive(Debug)]
+pub struct StepError {
+    history: Vec<String>,
+}
+
+pub fn step(prog: Language, current: &Value) -> Result<Value, StepError> {
     match prog {
-        Language::At(key) => current.get(key).unwrap().clone(),
-        Language::Focus(key, next) => step(*next, current.get(key).unwrap()),
-        Language::Array(next) => Value::Array(
+        Language::At(key) => Ok(current
+            .get(&key)
+            .ok_or_else(|| StepError { history: vec![key] })?
+            .clone()),
+        Language::Focus(key, next) => step(
+            *next,
+            current.get(&key).ok_or_else(|| StepError {
+                history: vec![key.clone()],
+            })?,
+        )
+        .map_err(|StepError { mut history }| StepError {
+            history: {
+                history.insert(0, key);
+                history
+            },
+        }),
+        Language::Array(next) => Ok(Value::Array(
             current
                 .as_array()
-                .unwrap()
+                .ok_or_else(|| StepError {
+                    history: vec![String::from("<Not an array>")],
+                })?
                 .iter()
-                .map(|x| step(*next.clone(), x))
-                .collect(),
-        ),
-        Language::Object(pairs) => Value::Object(
+                .enumerate()
+                .map(|(i, x)| {
+                    step(*next.clone(), x).map_err(|StepError { mut history }| StepError {
+                        history: {
+                            history.insert(0, format!("[{}]", i));
+                            history
+                        },
+                    })
+                })
+                .collect::<Result<Vec<Value>, StepError>>()?,
+        )),
+        Language::Object(pairs) => Ok(Value::Object(
             pairs
                 .into_iter()
-                .map(|(k, v)| (k, step(v, current)))
-                .collect(),
-        ),
+                .map(|(k, v)| step(v, current).map(|v| (k, v)))
+                .collect::<Result<Map<String, Value>, StepError>>()?,
+        )),
+    }
+}
+
+#[test]
+fn translate_error_at() {
+    use serde_json::json;
+    let prog = Language::At(String::from("foo"));
+
+    let given = json!({ "bar": "baz" });
+    if let Some(StepError { history }) = step(prog, &given).err() {
+        assert_eq!(history, vec!["foo"]);
+    }
+}
+
+#[test]
+fn translate_error_array() {
+    use serde_json::json;
+    let prog = Language::Array(Box::new(Language::At(String::from("foo"))));
+
+    let given = json!([{ "bar": "baz" }]);
+    if let Some(StepError { history }) = step(prog, &given).err() {
+        assert_eq!(history, vec!["[0]", "foo"]);
+    }
+}
+
+#[test]
+fn translate_error_focus() {
+    use serde_json::json;
+    let prog = Language::Focus(
+        String::from("foo"),
+        Box::new(Language::At(String::from("bar"))),
+    );
+
+    let given = json!({ "baz": "blix" });
+    if let Some(StepError { history }) = step(prog, &given).err() {
+        assert_eq!(history, vec!["foo"]);
+    }
+}
+
+#[test]
+fn translate_error_object() {
+    use serde_json::json;
+    let prog = Language::Object(vec![
+        (String::from("foo"), Language::At(String::from("foo"))),
+        (String::from("bar"), Language::At(String::from("bar"))),
+    ]);
+
+    let given = json!({ "foo": "foo" });
+    if let Some(StepError { history }) = step(prog, &given).err() {
+        assert_eq!(history, vec!["bar"]);
     }
 }
 
@@ -47,5 +126,5 @@ fn translate_test() {
     let given = json!({ "q": "Foo", "results": [{"product_variant_id": "12313bb7-6068-4ec9-ac49-3e834181f127"}] });
     let expected = json!({ "ids": [ "12313bb7-6068-4ec9-ac49-3e834181f127" ] });
 
-    assert_eq!(step(prog, &given), expected);
+    assert_eq!(step(prog, &given).unwrap(), expected);
 }
