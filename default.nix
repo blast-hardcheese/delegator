e89f866bfd67d9fdb58298a869d8fb986d03e67a
Devon Stewart <devon@appreciate.it>
2023-02-14 12:25:17 -0500

    Adding a default.nix file for supplying relevant deps in nix-darwin

---
+++ default.nix
@@ -0,0 +1,16 @@
+{ nixpkgs ? import <nixpkgs> {} }:
+
+let
+  pkgs = [
+    nixpkgs.openssl
+    nixpkgs.pkg-config
+    nixpkgs.darwin.apple_sdk.frameworks.Security
+    nixpkgs.darwin.libiconv
+    nixpkgs.curl
+  ];
+
+in
+  nixpkgs.stdenv.mkDerivation {
+    name = "env";
+    buildInputs = pkgs;
+  }
