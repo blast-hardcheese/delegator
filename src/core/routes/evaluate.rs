9d255e36e8eee2d5add5567bb17041d475530092
Devon Stewart <devon@appreciate.it>
2023-05-05 16:56:52 -0700

    Adding sentry breadcrumbs

--- src/core/routes/evaluate.rs
+++ src/core/routes/evaluate.rs
@@ -56,8 +56,17 @@ pub enum EvaluateError {
     UriBuilderError(error::HttpError),
     Utf8Error(Utf8Error),
 }
+
 impl JsonResponseError for EvaluateError {
     fn error_as_json(&self) -> Value {
+        fn breadcrumb(msg: &str) -> Breadcrumb {
+            Breadcrumb {
+                message: Some(String::from(msg)),
+                ty: String::from("evaluate_step"),
+                category: Some(String::from("error")),
+                ..Breadcrumb::default()
+            }
+        }
         fn err(msg: &str) -> Value {
             json!({
                "error": {
@@ -67,32 +76,98 @@ impl JsonResponseError for EvaluateError {
         }
         match self {
             Self::ClientError(inner) => {
+                sentry::add_breadcrumb(breadcrumb("ClientError"));
                 sentry::capture_error(inner);
                 err("client")
             }
             Self::InvalidJsonError(inner) => {
+                sentry::add_breadcrumb(breadcrumb("InvalidJsonError"));
                 sentry::capture_error(inner);
                 err("protocol")
             }
             Self::InvalidPayloadError(inner) => {
+                sentry::add_breadcrumb(breadcrumb("PayloadError"));
                 sentry::capture_error(inner);
                 err("payload")
             }
-            Self::UnknownStep(_num) => err("unknown_step"),
+            Self::UnknownStep(num) => {
+                sentry::add_breadcrumb({
+                    let mut b = breadcrumb("UnknownStep");
+                    b.data
+                        .insert(String::from("step"), Value::Number((*num).into()));
+                    b
+                });
+                err("unknown_step")
+            }
             Self::InvalidStructure(inner) => {
+                sentry::add_breadcrumb(breadcrumb("PayloadError"));
                 sentry::capture_error(inner);
                 err("payload")
             }
-            Self::InvalidTransition(_steps, _step) => err("unknown_transition"),
-            Self::NetworkError(_context) => err("network"),
-            Self::NoStepsSpecified => err("steps"),
-            Self::UnknownMethod(_service_name, _method_name) => err("unknown_method"),
-            Self::UnknownService(_service_name) => err("unknown_service"),
+            Self::InvalidTransition(steps, step) => {
+                sentry::add_breadcrumb({
+                    let mut b = breadcrumb("InvalidTransition");
+                    b.data.insert(
+                        String::from("steps"),
+                        Value::Array(steps.iter().map(|i| Value::Number((*i).into())).collect()),
+                    );
+                    b.data
+                        .insert(String::from("step"), Value::Number((*step).into()));
+                    b
+                });
+                err("unknown_transition")
+            }
+            Self::NetworkError(context) => {
+                sentry::add_breadcrumb({
+                    let mut b = breadcrumb("NetworkError");
+                    if let Value::Object(hm) = context {
+                        for (k, v) in hm.iter() {
+                            b.data.insert(k.clone(), v.clone());
+                        }
+                    } else {
+                        b.data.insert(String::from("_json"), context.clone());
+                    }
+                    b
+                });
+                err("network")
+            }
+            Self::NoStepsSpecified => {
+                sentry::add_breadcrumb(breadcrumb("NoStepsSpecified"));
+                err("steps")
+            }
+            Self::UnknownMethod(service_name, method_name) => {
+                sentry::add_breadcrumb({
+                    let mut b = breadcrumb("UnknownMethod");
+                    b.data.insert(
+                        String::from("service"),
+                        Value::String(service_name.to_string()),
+                    );
+                    b.data.insert(
+                        String::from("method"),
+                        Value::String(method_name.to_string()),
+                    );
+                    b
+                });
+                err("unknown_method")
+            }
+            Self::UnknownService(service_name) => {
+                sentry::add_breadcrumb({
+                    let mut b = breadcrumb("UnknownService");
+                    b.data.insert(
+                        String::from("service"),
+                        Value::String(service_name.to_string()),
+                    );
+                    b
+                });
+                err("unknown_service")
+            }
             Self::UriBuilderError(inner) => {
+                sentry::add_breadcrumb(breadcrumb("UriBuilderError"));
                 sentry::capture_error(inner);
                 err("unknown_service")
             }
             Self::Utf8Error(inner) => {
+                sentry::add_breadcrumb(breadcrumb("Utf8Error"));
                 sentry::capture_error(inner);
                 err("encoding")
             }
