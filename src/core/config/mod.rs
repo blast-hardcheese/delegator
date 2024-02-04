d03af1ef694204e6f51c01ced23eb663f8e93c08
Devon Stewart <blast@hardchee.se>
2024-02-04 14:55:32 +0000

    Switching from hocon to TOML

--- src/core/config/mod.rs
+++ src/core/config/mod.rs
@@ -7,13 +7,16 @@ mod stringy_duration;
 use std::time::Duration;
 
 use actix_web::http::{
-    uri::{Authority, PathAndQuery, Scheme},
     Method,
+    uri::{Authority, PathAndQuery, Scheme},
 };
 use derive_more::Display;
 use hashbrown::HashMap;
 
-use serde::Deserialize;
+use serde::{Deserialize, Deserializer};
+use serde_plain;
+
+use toml;
 
 use self::events::EventConfig;
 
@@ -40,20 +43,28 @@ pub enum MethodName {
     Lookup,
 }
 
+
 #[derive(Clone, Debug, Deserialize, Display, Eq, Hash, PartialEq)]
 pub enum ServiceName {
-    #[serde(rename(deserialize = "apex"))]
+    #[serde(rename = "apex")]
     Apex,
-    #[serde(rename(deserialize = "catalog"))]
+    #[serde(rename = "catalog")]
     Catalog,
-    #[serde(rename(deserialize = "closet"))]
+    #[serde(rename= "closet")]
     Closet,
-    #[serde(rename(deserialize = "identity"))]
+    #[serde(rename = "identity")]
     Identity,
-    #[serde(rename(deserialize = "pricing"))]
+    #[serde(rename = "pricing")]
     Pricing,
-    #[serde(rename(deserialize = "recommendations"))]
+    #[serde(rename = "recommendations")]
     Recommendations,
+}
+
+impl ServiceName {
+    pub fn from_str(name: &str) -> Result<Self, &'static str> {
+        let deserialized_name = serde_plain::from_str::<ServiceName>(name).map_err(|_| "No matching service name found")?;
+        Ok(deserialized_name)
+    }
 }
 
 #[derive(Clone, Debug, Deserialize)]
@@ -105,14 +116,27 @@ pub type Services = HashMap<ServiceName, ServiceDefinition>;
 #[derive(Clone, Debug, Deserialize)]
 pub struct Configuration {
     pub http: HttpConfig,
+    #[serde(deserialize_with = "deserialize_services")]
     pub services: Services,
     pub virtualhosts: Virtualhosts,
     pub events: EventConfig,
 }
 
+fn deserialize_services<'de, D>(deserializer: D) -> Result<Services, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let mut services: Services = HashMap::new();
+    let service_defs: HashMap<String, ServiceDefinition> = Deserialize::deserialize(deserializer)?;
+    for (key, value) in service_defs {
+        let service_name = ServiceName::from_str(&key).map_err(serde::de::Error::custom)?;
+        services.insert(service_name, value);
+    }
+    Ok(services)
+}
+
 pub fn load_file(path: &str) -> Result<Configuration, std::io::Error> {
     let config_str = std::fs::read_to_string(path)?;
-    let config: Configuration = toml::from_str(&config_str)
-        .map_err(|e| std::io::Error::new(std::io::ErrorKind::InvalidData, e.to_string()))?;
+    let config: Configuration = toml::from_str(&config_str).unwrap();
     Ok(config)
 }
