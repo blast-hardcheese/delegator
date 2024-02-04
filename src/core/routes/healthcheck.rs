3c18849459cb5d102341b065c016bf59780e1087
Devon Stewart <blast@hardchee.se>
2024-02-04 22:53:11 +0000

    Dropping catalog, closet, healthcheck, and pricing routes

--- src/core/routes/healthcheck.rs
+++
@@ -1,9 +0,0 @@
-use actix_web::{web, HttpResponse};
-
-pub async fn healthcheck() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-pub fn configure(server: &mut web::ServiceConfig) {
-    server.route("/health", web::get().to(healthcheck));
-}
--- src/core/routes/mod.rs
+++ src/core/routes/mod.rs
@@ -2,18 +2,9 @@ use actix_web::web;
 
 use crate::config::Virtualhosts;
 
-pub mod catalog;
-pub mod closet;
 pub mod errors;
 pub mod evaluate;
-pub mod healthcheck;
-pub mod pricing;
 
 pub fn configure(server: &mut web::ServiceConfig, virtualhosts: &Virtualhosts) {
-    server
-        .configure(evaluate::configure)
-        .configure(healthcheck::configure)
-        .configure(|server| catalog::configure(server, virtualhosts.catalog.clone()))
-        .configure(|server| closet::configure(server, virtualhosts.closet.clone()))
-        .configure(|server| pricing::configure(server, virtualhosts.pricing.clone()));
+    server.configure(evaluate::configure);
 }
